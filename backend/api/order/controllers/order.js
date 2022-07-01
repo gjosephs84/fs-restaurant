66983ce95aad1e83944b633fd09eef7836a0508a
Gregory Josephs <greg.josephs@gmail.com>
2022-07-01 07:26:48 -0400

    added stripe key

--- backend/api/order/controllers/order.js
+++ backend/api/order/controllers/order.js
@@ -1,8 +1,47 @@
 'use strict';
 
+"use strict";
+
 /**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
+ * Order.js controller
+ *
+ * @description: A set of functions called "actions" for managing `Order`.
  */
 
-module.exports = {};
+const stripe = require("stripe")("sk_test_51L85kiLaUsM4yOOuFPRbWpTwhlf3pAVyITjTxVBITbgrFibzgsDhGL6nqVwoH3IJNOi4iuHJ6PvVAMM1yU7NxfQu00dWd0qqRX");
+
+module.exports = {
+  /**
+   * Create a/an order record.
+   *
+   * @return {Object}
+   */
+
+  create: async (ctx) => {
+    const { address, amount, dishes, token, city, state } = JSON.parse(
+      ctx.request.body
+    );
+    const stripeAmount = Math.floor(amount * 100);
+    // charge on stripe
+    const charge = await stripe.charges.create({
+      // Transform cents to dollars.
+      amount: stripeAmount,
+      currency: "usd",
+      description: `Order ${new Date()} by ${ctx.state.user._id}`,
+      source: token,
+    });
+
+    // Register the order in the database
+    const order = await strapi.services.order.create({
+      user: ctx.state.user.id,
+      charge_id: charge.id,
+      amount: stripeAmount,
+      address,
+      dishes,
+      city,
+      state,
+    });
+
+    return order;
+  },
+};
